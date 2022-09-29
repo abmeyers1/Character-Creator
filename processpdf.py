f17ff8f7a7662560001604e4dbc673348b65000c
Alex Meyers <brumfial@oregonstate.edu>
2022-09-29 11:42:26 -0500

    Fixed saving sheet in correct folder

--- processpdf.py
+++ processpdf.py
@@ -66,7 +66,7 @@ def process(character):
                     writer.pages[0], {skill: character.stats[statlist[i]+'mod']}
                 )
     # write "output" to PyPDF2-output.pdf
-    with open(f"/Sheets/{character.char_name}_Level_1.pdf", "wb") as output_stream:
+    with open(f"./Sheets/{character.char_name}_Level_1.pdf", "wb") as output_stream:
         writer.write(output_stream)
     return
 # process('h')
